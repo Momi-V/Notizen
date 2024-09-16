fb9858f4813c5467a4a4a6c6afbdc0f52ca443b0
HPPinata <83947761+HPPinata@users.noreply.github.com>
2024-09-16 14:44:07 +0200

    move ROCm install to Dockerfile

--- rocm/reGen.bash
+++ rocm/reGen.bash
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-apt update && apt full-upgrade -y && apt autopurge -y
-apt install -y curl git nano wget
-apt install -y libjemalloc2
-wget -r -nd -np -A 'amdgpu-install*all.deb' "https://repo.radeon.com/amdgpu-install/latest/ubuntu/noble/"
-apt install -y ./amdgpu-install*all.deb
-amdgpu-install -y --usecase=rocm --no-dkms
-rocminfo
-
 cd /home/rocm-user/
 git clone https://github.com/Haidra-Org/horde-worker-reGen.git
 cd horde-worker-reGen/
