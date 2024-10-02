e68871b185b080f537a572aa7eedd8887bada455
HPPinata <83947761+HPPinata@users.noreply.github.com>
2024-10-02 16:47:42 +0200

    Create extrainstall.bash

---
+++ openSUSE/extrainstall.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+for i in $(zypper pa --userinstalled | awk -F'|' 'NR==0 || NR==1 || NR==2 || NR==3 || NR==4 {next} {print $3}' | uniq); do
+  if [[ $(zypper se -i --requires-pkg $i | grep -v " $i " | grep 'i.*|') ]]; then
+    echo; echo $i:; echo;
+    zypper se -i --requires-pkg $i;
+    echo ==============================;
+  fi
+done
