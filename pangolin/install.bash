4f19e005fec1fe17f660f8e4aa0a6c2441ccccf9
HPPinata <83947761+HPPinata@users.noreply.github.com>
2025-03-27 21:07:28 +0100

    Create install.bash

---
+++ pangolin/install.bash
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+mkdir /var/pangolin
+cd /var/pangolin
+
+wget -O installer "https://github.com/fosrl/pangolin/releases/latest/download/installer_linux_$(uname -m | sed 's/x86_64/amd64/;s/aarch64/arm64/')" && chmod +x ./installer
+./installer
+
+sed -i 's+image: fosrl/pangolin:.*+image: fosrl/pangolin:latest+g' docker-compose.yml
+sed -i 's+image: fosrl/gerbil:.*+image: fosrl/gerbil:latest+g' docker-compose.yml
+sed -i 's+image: traefik:.*+image: traefik:v3+g' docker-compose.yml
+
+cat <<'EOL' > update.bash
+#!/bin/bash
+cd /var/pangolin
+docker compose pull
+docker compose build --pull
+docker compose up -dV
+docker system prune -a -f --volumes
+EOL
+cat update.bash
+chmod +x update.bash
+
+cat <<'EOL' | crontab -
+SHELL=/bin/bash
+BASH_ENV=/etc/profile
+
+@reboot /var/pangolin/update.bash
+EOL
+
+crontab -l
