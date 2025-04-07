e698d69c158d12b82b4352a75a7171f0ca21dc18
HPPinata <83947761+HPPinata@users.noreply.github.com>
2025-04-07 22:46:44 +0200

    Create setup.bash

---
+++ pangolin/setup.bash
@@ -0,0 +1,21 @@
+cat <<'EOL' | sfdisk /dev/vda
+size=3M type=4
+size=509M
+size=40G
++ type=swap
+EOL
+
+mkfs.fat -F 32 /dev/vda2
+fatlabel /dev/vda2 NIXBOOT
+mkfs.btrfs /dev/vda3 -L NIXROOT
+mkswap /dev/vda4
+swapon /dev/vda4
+
+mount /dev/disk/by-label/NIXROOT /mnt
+mkdir -p /mnt/boot
+mount /dev/disk/by-label/NIXBOOT /mnt/boot
+
+nixos-generate-config --root /mnt
+wget -O /mnt/etc/nixos/configuration.nix https://raw.githubusercontent.com/HPPinata/Notizen/refs/heads/main/pangolin/configuration.nix
+cd /mnt
+nixos-install
