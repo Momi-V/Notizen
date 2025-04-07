08f395f06e80d449d97b76123e8c925eb0d7b665
HPPinata <83947761+HPPinata@users.noreply.github.com>
2025-04-07 23:41:13 +0200

    echo and SSH

--- pangolin/setup.bash
+++ pangolin/setup.bash
@@ -1,7 +1,10 @@
+echo "Wiping Drives"
 swapoff -a
 umount /dev/vda*
 wipefs -a /dev/vda*
+sleep 1
 
+echo "Creating Partitions"
 cat <<'EOL' | sfdisk /dev/vda
 label: gpt
 size=3M type=21686148-6449-6E6F-744E-656564454649
@@ -9,20 +12,33 @@ size=509M
 size=40G
 type=swap
 EOL
+sleep 1
 
+echo "Formatting Filesystems"
 mkfs.fat -F 32 /dev/vda2
 fatlabel /dev/vda2 NIXBOOT
 mkfs.btrfs /dev/vda3 -L NIXROOT
 mkswap /dev/vda4
-swapon /dev/vda4
+sleep 1
 
+echo "Mounting Filesystems"
+swapon /dev/vda4
 mount /dev/disk/by-label/NIXROOT /mnt
 mkdir -p /mnt/boot
 mount /dev/disk/by-label/NIXBOOT /mnt/boot
+sleep 1
 
+echo "Generating and Fetching config"
 nixos-generate-config --root /mnt
 curl -o /mnt/etc/nixos/configuration.nix https://raw.githubusercontent.com/HPPinata/Notizen/refs/heads/main/pangolin/configuration.nix
+sleep 1
 
+echo "Installing NixOS"
 cd /mnt
 nix-channel --update
 nixos-install
+sleep 1
+
+echo "Copying root SSH keys"
+cp -r /root/.ssh/ /mnt/root/
+sleep 1
