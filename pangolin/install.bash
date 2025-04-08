7454dadea5f4fac732d4479ae26850f6d98cb4d0
HPPinata <83947761+HPPinata@users.noreply.github.com>
2025-04-08 04:47:22 +0200

    git config

--- pangolin/install.bash
+++ pangolin/install.bash
@@ -3,6 +3,8 @@
 mkdir /var/pangolin
 cd /var/pangolin
 
+git init
+
 wget -O dyndns.bash "https://raw.githubusercontent.com/HPPinata/Notizen/refs/heads/main/pangolin/dyndns.bash" && chmod +x ./dyndns.bash
 wget -O installer "https://github.com/fosrl/pangolin/releases/latest/download/installer_linux_$(uname -m | sed 's/x86_64/amd64/;s/aarch64/arm64/')" && chmod +x ./installer
 
@@ -17,6 +19,9 @@ EOL
 ./dyndns.bash
 ./installer
 
+git add --all
+git commit -m installer
+
 sed -i 's+image: fosrl/pangolin:.*+image: fosrl/pangolin:latest+g' docker-compose.yml
 sed -i 's+image: fosrl/gerbil:.*+image: fosrl/gerbil:latest+g' docker-compose.yml
 sed -i 's+image: traefik:.*+image: traefik:v3+g' docker-compose.yml
@@ -24,6 +29,8 @@ sed -i 's+image: traefik:.*+image: traefik:v3+g' docker-compose.yml
 cat <<'EOL' > update.bash
 #!/bin/bash
 cd /var/pangolin
+git add --all
+git commit -m $(date "+%Y%m%d%H%M%S")
 docker compose pull
 docker compose build --pull
 docker compose up -d
@@ -31,3 +38,6 @@ docker system prune -a -f
 EOL
 cat update.bash
 chmod +x update.bash
+
+git add --all
+git commit -m autoupdate
