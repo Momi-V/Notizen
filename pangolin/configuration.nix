ef6ac4825e876ed6851b8a078181229f16674b39
HPPinata <83947761+HPPinata@users.noreply.github.com>
2025-07-21 02:06:53 +0200

    remove network manager

--- pangolin/configuration.nix
+++ pangolin/configuration.nix
@@ -21,8 +21,8 @@
   networking.hostName = "pangolin"; # Define your hostname.
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
-  networking.networkmanager.settings.connection.autoconnect = true; # Make sure autoconnect is active.
+  # networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  # networking.networkmanager.settings.connection.autoconnect = true; # Make sure autoconnect is active.
 
   # Set your time zone.
   time.timeZone = "Europe/Berlin";
