fc9fbb88803520dade9759c56ff66ebebcde1567
HPPinata <83947761+HPPinata@users.noreply.github.com>
2025-07-26 21:30:14 +0200

    eval array

--- nextcloud/sync.bash
+++ nextcloud/sync.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+eval "LOCALDIR=$LOCALDIR"
+eval "REMOTEDIR=$REMOTEDIR"
+
 for i in "${!LOCALDIR[@]}"; do
   nextcloudcmd --exclude "$EXCLUDEFILE" --path "${REMOTEDIR[i]}" "${LOCALDIR[i]}" https://"$USERNAME":"$PASSWORD"@"$SERVER"
 done
