73b28d560a8367f6b46b5d28bb279057cd05cf9d
HPPinata <83947761+HPPinata@users.noreply.github.com>
2025-07-26 22:00:52 +0200

    fixed exclude file

--- nextcloud/sync.bash
+++ nextcloud/sync.bash
@@ -4,7 +4,7 @@ eval "LOCALDIR=$LOCALDIR"
 eval "REMOTEDIR=$REMOTEDIR"
 
 for i in "${!LOCALDIR[@]}"; do
-  nextcloudcmd --exclude "$EXCLUDEFILE" --path "${REMOTEDIR[i]}" "${LOCALDIR[i]}" https://"$USERNAME":"$PASSWORD"@"$SERVER"
+  nextcloudcmd --exclude "${LOCALDIR[i]}/.sync-exclude.lst" --path "${REMOTEDIR[i]}" "${LOCALDIR[i]}" https://"$USERNAME":"$PASSWORD"@"$SERVER"
 done
 
 sleep $SLEEP
