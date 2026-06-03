c8b2f4ddf84c66879db78467bc1b021394e798f1
Momi-V <83947761+Momi-V@users.noreply.github.com>
2026-06-03 21:30:22 +0200

    close unused ports

--- pangolin/configuration.nix
+++ pangolin/configuration.nix
@@ -87,8 +87,8 @@
   };
 
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [ 22 80 443 3478 28967 28968 27015 ];
-  networking.firewall.allowedUDPPorts = [ 3478 28967 28968 34197 21820 51820 ];
+  networking.firewall.allowedTCPPorts = [ 22 80 443 ];
+  networking.firewall.allowedUDPPorts = [ 21820 51820 ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 
